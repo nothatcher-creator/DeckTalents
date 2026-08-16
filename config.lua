9b8aa0eeb0d518195455de6157dce4610276f374
nothatcher-creator <nothatcher@gmail.com>
2026-08-16 15:36:42 -0300

    Add default configuration

---
+++ config.lua
@@ -0,0 +1,17 @@
+return {
+    version = 16,
+    points = 0,
+    total_earned = 0,
+    unlocked = {},
+    global_unlocked = {},
+    global_cosmetics_enabled = {},
+    disabled_talents = {},
+    global_disabled_talents = {},
+    ante_scaling_enabled = true,
+    ui = {
+        last_deck = "b_red",
+        global_page = 1,
+        shop_page = 1,
+        deck_pages = {},
+    },
+}
